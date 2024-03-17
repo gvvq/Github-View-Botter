acfa6f22f57230cb467a35a61e7bd80b3ce2f21e
ohhq <135171695+gvvq@users.noreply.github.com>
2024-03-17 12:25:37 -0400

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,25 @@
+import asyncio
+import aiohttp
+import time
+
+async def make_request(session, url, semaphore):
+    async with semaphore:
+        async with session.get(url) as response:
+            if response.status == 200:
+                print("Request successful")
+
+async def main():
+    url = "https://visitor-badge.laobi.icu/badge?page_id=gvvq.gvvq"  # CHANGE THIS WITH UR LINK!!!!!!
+    num_requests = 1000000
+    concurrency_limit = 1000
+    start_time = time.time()
+    semaphore = asyncio.Semaphore(concurrency_limit)
+    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(limit=None)) as session:
+        tasks = [make_request(session, url, semaphore) for _ in range(num_requests)]
+        await asyncio.gather(*tasks)
+    end_time = time.time()
+    total_time = end_time - start_time
+    print(f"Took {total_time} seconds")
+
+if __name__ == "__main__":
+    asyncio.run(main())
